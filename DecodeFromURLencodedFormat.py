ff0216d82526a9b8ca1215fdd347c77d089fc659
Caliari Jr <Calliari@users.noreply.github.com>
2019-08-09 15:42:48 +0100

    Create DecodeFromURLencodedFormat.py

---
+++ DecodeFromURLencodedFormat.py
@@ -0,0 +1,6 @@
+####Decode from URL encoded format website
+https://www.urldecoder.org/
+
+####From terminal commands
+echo "%21%20" | python -c "import sys, urllib as ul; print ul.unquote(sys.stdin.read());"
+
