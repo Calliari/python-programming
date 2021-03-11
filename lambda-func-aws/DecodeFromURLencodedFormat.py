cf906687ae65ad670afb0cafb9e63c066ade41da
Caliari Jr <Calliari@users.noreply.github.com>
2021-03-11 20:49:41 +0000

    Delete lambda-func-aws directory

--- lambda-func-aws/DecodeFromURLencodedFormat.py
+++
@@ -1,6 +0,0 @@
-####Decode from URL encoded format website
-https://www.urldecoder.org/
-
-####From terminal commands
-echo "%21%20" | python -c "import sys, urllib as ul; print ul.unquote(sys.stdin.read());"
-
